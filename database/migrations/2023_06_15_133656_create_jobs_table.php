32e4ef2490b7452e8e958784052e95ac1ab23ac2
Jin <genesisjinsaquibal@gmail.com>
2023-06-17 09:20:52 +0800

    initialized table columns

--- database/migrations/2023_06_15_133656_create_jobs_table.php
+++ database/migrations/2023_06_15_133656_create_jobs_table.php
@@ -4,24 +4,30 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(): void {
         Schema::create('jobs', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 255);
+            $table->unsignedBigInteger('company_id');
+            $table->string('location', 255);
+            $table->text('description');
+            $table->text('requirements');
+            $table->string('salary', 50);
+            $table->dateTime('posted_at');
             $table->timestamps();
+
+            $table->foreign('company_id')->references('id')->on('companies');
         });
     }
 
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('jobs');
     }
 };
