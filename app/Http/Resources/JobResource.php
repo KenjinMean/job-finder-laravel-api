ae1d10a60c0667d779d08e3ccbc54cd4d24584ed
Jin <genesisjinsaquibal@gmail.com>
2023-06-17 09:17:26 +0800

    created JobResource

---
+++ app/Http/Resources/JobResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class JobResource extends JsonResource {
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'company_id' => $this->company_id,
+            'location' => $this->location,
+            'description' => $this->description,
+            'requirements' => $this->requirements,
+            'salary' => $this->salary,
+            'posted_at' => $this->posted_at->format('Y-m-d'),
+        ];
+    }
+}
