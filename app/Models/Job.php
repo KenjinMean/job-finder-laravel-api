f7e223396009d118d6e5623bf6c9963210894245
Jin <genesisjinsaquibal@gmail.com>
2023-06-17 09:19:34 +0800

    modified, added relationship to company, casts

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -5,7 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Job extends Model
-{
+class Job extends Model {
     use HasFactory;
+
+    protected $casts = [
+        'posted_at' => 'datetime:Y-m-d H:i:s',
+    ];
+
+    public function company() {
+        return $this->belongsTo(Company::class);
+    }
 }
