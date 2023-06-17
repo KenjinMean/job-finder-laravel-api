e0b353b69d374d6d1902d82ae8890a05c6a081c1
Jin <genesisjinsaquibal@gmail.com>
2023-06-17 09:20:20 +0800

    created job factory

---
+++ database/factories/JobFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Company;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Job>
+ */
+class JobFactory extends Factory {
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array {
+        return [
+            'title' => fake()->jobTitle,
+            'company_id' => function () {
+                return Company::inRandomOrder()->first()->id;
+            },
+            'location' => fake()->city,
+            'description' => fake()->paragraph,
+            'requirements' => fake()->paragraph,
+            'salary' => fake()->randomNumber(5),
+            'posted_at' => fake()->dateTime,
+            'updated_at' => fake()->dateTime,
+        ];
+    }
+}
