e75ecdb713d7c2a39d681896a2da564e3ccdc791
Jin <genesisjinsaquibal@gmail.com>
2023-06-17 09:22:50 +0800

    created api resource route for job

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,8 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\Api\JobController;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,4 +24,5 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
+    Route::apiResource('/jobs', JobController::class);
 });
