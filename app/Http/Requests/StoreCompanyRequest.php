744716cb091edbb93757bbf42bb79d8c74ee43bb
Jin <genesisjinsaquibal@gmail.com>
2023-06-17 18:34:31 +0800

    added validation rules

--- app/Http/Requests/StoreCompanyRequest.php
+++ app/Http/Requests/StoreCompanyRequest.php
@@ -4,14 +4,12 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreCompanyRequest extends FormRequest
-{
+class StoreCompanyRequest extends FormRequest {
     /**
      * Determine if the user is authorized to make this request.
      */
-    public function authorize(): bool
-    {
-        return false;
+    public function authorize(): bool {
+        return true;
     }
 
     /**
@@ -19,10 +17,14 @@ class StoreCompanyRequest extends FormRequest
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
      */
-    public function rules(): array
-    {
+    public function rules(): array {
         return [
-            //
+            // 'user_id' => 'required',
+            'name' => 'required',
+            'website'  => 'required',
+            'location'  => 'required',
+            'description'  => 'required',
+            'industry' => 'required',
         ];
     }
 }
