35df15564497460f945863b5ff2e023dc5770a0c
Jin <genesisjinsaquibal@gmail.com>
2023-06-24 10:45:01 +0800

    Commit Title: refactored index, implemented store Commit Description: refactore index directly use eloquent inside of resource collection,  implemented store function to store newly created jobs

--- app/Http/Controllers/Api/JobController.php
+++ app/Http/Controllers/Api/JobController.php
@@ -4,50 +4,66 @@ namespace App\Http\Controllers\Api;
 
 use App\Models\Job;
 use Illuminate\Http\Request;
-use App\Http\Resources\JobResource;
-use App\Http\Controllers\Controller;
+use Illuminate\Http\Response;
 use Illuminate\Http\JsonResponse;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreJobRequest;
+use App\Http\Resources\JobPreliminaryResource;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class JobController extends Controller {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index(): AnonymousResourceCollection {
-        $jobs = Job::with('company')
-            ->orderBy('id', 'desc')
-            ->paginate();
+  /**
+   * Display a listing of the resource.
+   */
+  public function index(): AnonymousResourceCollection {
+    return JobPreliminaryResource::collection(Job::with('company', 'category')
+      ->orderBy('posted_at', 'desc')
+      ->paginate(10));
+  }
 
-        return JobResource::collection($jobs);
-        // return JobResource::collection(Job::query()->orderBy('id', 'desc')->paginate());
-    }
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store(StoreJobRequest $request) {
+    $validated = $request->validated();
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request) {
-        //
-    }
+    try {
+      $job = Job::create([
+        'company_id' => $validated['company_id'],
+        'category_id' => $validated['category_id'],
+        'title' => $validated['title'],
+        'location' => $validated['location'],
+        'description' => $validated['description'],
+        'requirements' => $validated['requirements'],
+        'salary' => $validated['salary'],
+        'posted_at' => now(),
+      ]);
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id): JsonResponse {
-        $job = Job::with('company')->findOrFail($id);
-        return response()->json($job);
+      return response()->json(['message' => 'Company created successfully', 'company' => $job]);
+    } catch (\Exception $e) {
+      return response()->json(['message' => 'Error creating job', 'error' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
     }
+  }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id) {
-        //
-    }
+  /**
+   * Display the specified resource.
+   */
+  public function show(string $id): JsonResponse {
+    $job = Job::with('company')->findOrFail($id);
+    return response()->json($job);
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id) {
-        //
-    }
+  /**
+   * Update the specified resource in storage.
+   */
+  public function update(Request $request, string $id) {
+    //
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   */
+  public function destroy(string $id) {
+    //
+  }
 }
