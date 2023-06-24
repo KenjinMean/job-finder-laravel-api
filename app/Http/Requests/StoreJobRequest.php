2ee82d1219f66ca019972f853a7cb7151d5a55fb
Jin <genesisjinsaquibal@gmail.com>
2023-06-24 10:51:37 +0800

    Commit Title: setup rules for request

--- app/Http/Requests/StoreJobRequest.php
+++ app/Http/Requests/StoreJobRequest.php
@@ -4,14 +4,12 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreJobRequest extends FormRequest
-{
+class StoreJobRequest extends FormRequest {
     /**
      * Determine if the user is authorized to make this request.
      */
-    public function authorize(): bool
-    {
-        return false;
+    public function authorize(): bool {
+        return true;
     }
 
     /**
@@ -19,10 +17,15 @@ class StoreJobRequest extends FormRequest
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
      */
-    public function rules(): array
-    {
+    public function rules(): array {
         return [
-            //
+            'company_id' => 'required',
+            'category_id' => 'required',
+            'title' => 'required',
+            'location' => 'required',
+            'description' => 'required',
+            'requirements' => 'required',
+            'salary' => 'required',
         ];
     }
 }
