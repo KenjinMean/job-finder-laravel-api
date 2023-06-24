805ca4c12af290e0ad8a55b22e4b299a023fb088
Jin <genesisjinsaquibal@gmail.com>
2023-06-24 11:00:23 +0800

    Commit Title: included category_id and posted at to fake()

--- database/factories/JobFactory.php
+++ database/factories/JobFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Category;
 use App\Models\Company;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -20,11 +21,14 @@ class JobFactory extends Factory {
             'company_id' => function () {
                 return Company::inRandomOrder()->first()->id;
             },
+            'category_id' => function () {
+                return Category::inRandomOrder()->first()->id;
+            },
             'location' => fake()->city,
             'description' => fake()->paragraph,
             'requirements' => fake()->paragraph,
             'salary' => fake()->randomNumber(5),
-            'posted_at' => fake()->dateTime,
+            'posted_at' => fake()->dateTimeBetween('-1 month', 'now'),
             'updated_at' => fake()->dateTime,
         ];
     }
