425c1b72fe450b384296503c87df99b2fc2e0858
Jin <genesisjinsaquibal@gmail.com>
2023-06-25 13:01:29 +0800

    Commit Title: added 'user/company' route for getCompany

--- routes/api.php
+++ routes/api.php
@@ -21,15 +21,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
 
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('logout', [AuthController::class, 'logout']);
+
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
+
+    Route::get('users/company', [UserController::class, 'getCompany']);
+
     Route::apiResource('/jobs', JobController::class);
     Route::apiResource('/company', CompanyController::class);
     Route::apiResource('/category', CategoryController::class);
     Route::apiResource('/users', UserController::class);
+
+    Route::post('logout', [AuthController::class, 'logout']);
 });
