c9a3fa3cd8ec9138c00285906eb742a694b9f98c
Jin <genesisjinsaquibal@gmail.com>
2023-06-25 12:58:15 +0800

    Commit Title: set authorization to true, set request rules

--- app/Http/Requests/RegisterUserRequest.php
+++ app/Http/Requests/RegisterUserRequest.php
@@ -4,14 +4,12 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class RegisterUserRequest extends FormRequest
-{
+class RegisterUserRequest extends FormRequest {
     /**
      * Determine if the user is authorized to make this request.
      */
-    public function authorize(): bool
-    {
-        return false;
+    public function authorize(): bool {
+        return true;
     }
 
     /**
@@ -19,10 +17,12 @@ class RegisterUserRequest extends FormRequest
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
      */
-    public function rules(): array
-    {
+    public function rules(): array {
         return [
-            //
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required',
+            'confirm_password' => 'required|same:password',
         ];
     }
 }
