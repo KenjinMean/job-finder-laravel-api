39461a0e3b4068af72d46e98169220a8ead00f7f
Jin <genesisjinsaquibal@gmail.com>
2023-06-25 12:56:42 +0800

    Commit Title: implement index, store, getCompany method Commit description: getCompany method to get all company associated with the user

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\RegisterUserRequest;
 use App\Http\Resources\UserPartialResource;
 
 class UserController extends Controller {
@@ -20,16 +22,19 @@ class UserController extends Controller {
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request) {
-
-        return response(['message' => 'ok']);
+    public function store(RegisterUserRequest $request) {
+        $validatedData = $request->validated();
+        $user = User::create($validatedData);
+        return response()->json(['message' => $user]);
     }
 
     /**
      * Display the specified resource.
      */
     public function show(string $id) {
-        //
+        $user = User::with('company')->findOrFail($id);
+        // return UserPartialResource::collection($user);
+        return response()->json(['message' => 'Ok', 'user' => $user]);
     }
 
     /**
@@ -44,5 +49,16 @@ class UserController extends Controller {
      */
     public function destroy(string $id) {
         //
+    }
+
+    public function getCompany() {
+        $user = Auth::user();
+
+        if (!$user) {
+            return response()->json(['message' => 'User not authenticated'], 401);
+        }
+
+        $company = $user->company;
+        return response()->json($company);
     }
 }
