c4cf215273794ae32d0928beb81e7d243a1e1dd3
Jin <genesisjinsaquibal@gmail.com>
2023-06-25 12:54:08 +0800

    Commit Title: added function register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,12 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Http\Requests\LoginRequest;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\RegisterUserRequest;
 
 class AuthController extends Controller {
+
+    public function register(RegisterUserRequest $request) {
+        $validatedData = $request->validated();
+        $user = User::create($validatedData);
+
+        // Automatically log in the user
+        Auth::login($user);
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response(compact('user', 'token'));
+    }
+
     public function login(LoginRequest $request) {
         $credentials = $request->validated();
         if (!Auth::attempt($credentials)) {
@@ -24,10 +38,7 @@ class AuthController extends Controller {
     }
 
     public function logout(Request $request) {
-        /** @var User $user */
-        $user = $request->user();
-        $user->currentAccessToken()->delete();
-
+        $request->user()->currentAccessToken()->delete();
         return response(['message' => 'User logged out successfully.'], Response::HTTP_NO_CONTENT);
     }
 }
