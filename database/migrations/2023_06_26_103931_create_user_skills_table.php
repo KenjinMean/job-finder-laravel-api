ef5bae60ce50c18bc305b6e14bf016b8344391d8
Jin <genesisjinsaquibal@gmail.com>
2023-06-26 10:45:40 +0800

    Commit Title: setup neccessary column and relationship

--- database/migrations/2023_06_26_103931_create_user_skills_table.php
+++ database/migrations/2023_06_26_103931_create_user_skills_table.php
@@ -4,15 +4,17 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(): void {
         Schema::create('user_skills', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->unsignedBigInteger('skill_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('skill_id')->references('id')->on('skills')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -20,8 +22,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('user_skills');
     }
 };
