d67dfb6b5b3b60d09dcb26bdac66c2e6f7351e17
Jin <genesisjinsaquibal@gmail.com>
2023-06-29 11:57:55 +0800

    Commit Title: created user_infos column

--- database/migrations/2023_06_29_113850_create_user_infos_table.php
+++ database/migrations/2023_06_29_113850_create_user_infos_table.php
@@ -4,15 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(): void {
         Schema::create('user_infos', function (Blueprint $table) {
             $table->id();
+            $table->string('headline');
+            $table->string('firstName');
+            $table->string('lastName');
+            $table->string('additionalName');
+            $table->string('pronouns');
+            $table->text('about');
+            $table->string('location');
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -20,8 +27,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('user_infos');
     }
 };
