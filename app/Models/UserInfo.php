84a9ef9de0862b671b2868037bf98574adf9c064
Jin <genesisjinsaquibal@gmail.com>
2023-07-03 23:10:50 +0800

    Commit Title: update userinfo fillable fields

--- app/Models/UserInfo.php
+++ app/Models/UserInfo.php
@@ -5,7 +5,28 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class UserInfo extends Model
-{
+class UserInfo extends Model {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        "firstName",
+        "lastName",
+        "headline",
+        "additionalName",
+        "pronouns",
+        "about",
+        "location",
+        "user_id",
+        "profile_image",
+        "cover_image",
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
