7a1bc5c14b65f54e6e597319ba91bf90f29045eb
Jin <genesisjinsaquibal@gmail.com>
2023-07-03 23:12:46 +0800

    Commit Title: refactored, used custom routes instead of Resource routes

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,13 @@
 <?php
 
-use App\Http\Controllers\Api\CategoryController;
-use App\Http\Controllers\Api\CompanyController;
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\Api\JobController;
-use App\Http\Controllers\Api\UserController;
-use App\Http\Resources\CategoryResource;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\JobController;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\CompanyController;
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\UserInfoController;
+use App\Http\Controllers\Api\UserSkillController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,18 +26,41 @@ Route::post('register', [AuthController::class, 'register']);
 
 Route::middleware('auth:sanctum')->group(function () {
 
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
-
     Route::apiResource('/jobs', JobController::class);
-    Route::apiResource('/company', CompanyController::class);
     Route::apiResource('/category', CategoryController::class);
 
-    Route::get('user/info', [UserController::class, 'getInfo']);
-    Route::get('user/companies', [UserController::class, 'getCompany']);
-    Route::post('user/set-skills', [UserController::class, 'setSkill']);
-    Route::apiResource('/user', UserController::class);
+    Route::post('set-skills', [UserSkillController::class, 'setSkill']);
+
+
+    #user routes
+    Route::prefix('users')->group(function () {
+        Route::get('/', [UserController::class, 'index'])->name('user.index');
+        Route::post('/store', [UserController::class, 'store'])->name('user.store');
+        Route::get('/show', [UserController::class, 'show'])->name('user.show');
+        Route::put('/update', [UserController::class, 'update'])->name('user.update');
+        Route::delete('/destroy', [UserController::class, 'destroy'])->name('user.destroy');
+    });
+
+    #user Company routes
+    Route::prefix('companies')->group(function () {
+        Route::get('/', [CompanyController::class, 'index'])->name('companies.index');
+        Route::post('/store', [CompanyController::class, 'store'])->name('companies.store');
+        Route::get('/show', [CompanyController::class, 'show'])->name('companies.show');
+        Route::put('/update', [CompanyController::class, 'update'])->name('companies.update');
+        Route::delete('/destroy', [CompanyController::class, 'destroy'])->name('companies.destroy');
+        Route::get('/user-companies', [CompanyController::class, 'getCompany'])->name('companies.user-companies');
+    });
+
+    #user Info routes
+    Route::prefix('user-infos')->group(function () {
+        Route::get('/', [UserInfoController::class, 'index'])->name('user-infos.index');
+        Route::post('/store', [UserInfoController::class, 'store'])->name('user-infos.store');
+        Route::get('/show', [UserInfoController::class, 'show'])->name('user-infos.show');
+        Route::put('/update', [UserInfoController::class, 'update'])->name('user-infos.update');
+        Route::delete('/delete', [UserInfoController::class, 'destroy'])->name('user-infos.destroy');
+        Route::patch('/update-profile', [UserInfoController::class, 'updateProfile'])->name('user-infos.update-profile');
+        Route::patch('/update-cover', [UserInfoController::class, 'updateCover'])->name('user-infos.update-cover');
+    });
 
     Route::post('logout', [AuthController::class, 'logout']);
 });
