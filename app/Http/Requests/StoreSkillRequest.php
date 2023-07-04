3601f5db7e91ebf3b06b75c2740272e2338108df
Jin <genesisjinsaquibal@gmail.com>
2023-07-04 22:20:21 +0800

    Commit Title: fixed typo "requierd"

--- app/Http/Requests/StoreSkillRequest.php
+++ app/Http/Requests/StoreSkillRequest.php
@@ -19,7 +19,7 @@ class StoreSkillRequest extends FormRequest {
      */
     public function rules(): array {
         return [
-            "skill" => "requierd|string"
+            "skill" => "required|string"
         ];
     }
 }
