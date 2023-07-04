92e7e82903cc09d44c42655ef8f321f969ada631
Jin <genesisjinsaquibal@gmail.com>
2023-07-04 22:13:58 +0800

    Commit Title: added name as fillable

--- app/Models/Skill.php
+++ app/Models/Skill.php
@@ -8,6 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Skill extends Model {
     use HasFactory;
 
+    protected $fillable = [
+        "name",
+    ];
+
     public function users() {
         return $this->belongsToMany(User::class, 'user_skill')->withTimestamps();
     }
