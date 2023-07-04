44eeb579658b575c04465b0bffa02492d3c10517
Jin <genesisjinsaquibal@gmail.com>
2023-07-04 22:17:38 +0800

    Commit Title: modified index and store methods

--- app/Http/Controllers/Api/SkillController.php
+++ app/Http/Controllers/Api/SkillController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Models\Skill;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\SkillResource;
 use App\Http\Requests\StoreSkillRequest;
 use App\Http\Requests\UpdateSkillRequest;
 
@@ -12,14 +13,17 @@ class SkillController extends Controller {
      * Display a listing of the resource.
      */
     public function index() {
-        //
+        $skills = Skill::all();
+        return SkillResource::collection($skills);
     }
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(StoreSkillRequest $request) {
-        //
+        $validatedSkill = $request->validated();
+        Skill::create($validatedSkill);
+        return response()->json(['message' => "skill created successfully"]);
     }
 
     /**
