862931e72cdaf8220bf936b0d9d1791a537fbc9a
Jin <genesisjinsaquibal@gmail.com>
2023-07-07 22:38:26 +0800

    Commit Title: created CompanyService

---
+++ app/Services/CompanyService.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Services;
+
+class CompanyService {
+  public function createUser(array $data) {
+    // Logic to create a user
+  }
+
+  public function updateUser(int $userId, array $data) {
+    // Logic to update a user
+  }
+
+  // Other methods related to user operations
+}
