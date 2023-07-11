3edc0b87df519105688443315864b8f30a1cc624
Jin <genesisjinsaquibal@gmail.com>
2023-07-11 13:17:10 +0800

    modified JobController, use Jobservice and Exception helper

--- app/Http/Controllers/Api/JobController.php
+++ app/Http/Controllers/Api/JobController.php
@@ -2,78 +2,104 @@
 
 namespace App\Http\Controllers\Api;
 
+use Throwable;
 use App\Models\Job;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
+use App\Services\JobService;
+use App\Helpers\ExceptionHelper;
 use Illuminate\Http\JsonResponse;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\SearchJobRequest;
 use App\Http\Requests\StoreJobRequest;
-use App\Http\Resources\JobPreliminaryResource;
-use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use App\Http\Requests\UpdateJobRequest;
+use App\Http\Requests\UpdateJobSkillRequest;
 
 class JobController extends Controller {
-  /**
-   * Display a listing of the resource.
-   */
-  public function index(): AnonymousResourceCollection {
-    return JobPreliminaryResource::collection(Job::with('company', 'skills')
-      ->orderBy('posted_at', 'desc')
-      ->paginate(10));
-  }
 
-  /**
-   * Store a newly created resource in storage.
-   */
-  public function store(StoreJobRequest $request) {
-    $validated = $request->validated();
+    private $jobService;
 
-    try {
-      $job = Job::create([
-        'company_id' => $validated['company_id'],
-        'category_id' => $validated['category_id'],
-        'title' => $validated['title'],
-        'location' => $validated['location'],
-        'description' => $validated['description'],
-        'requirements' => $validated['requirements'],
-        'salary' => $validated['salary'],
-        'posted_at' => now(),
-      ]);
-
-      return response()->json(['message' => 'Company created successfully', 'company' => $job]);
-    } catch (\Exception $e) {
-      return response()->json(['message' => 'Error creating job', 'error' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+    public function __construct(JobService $jobService) {
+        $this->jobService = $jobService;
     }
-  }
 
-  /**
-   * Display the specified resource.
-   */
-  public function show(string $id): JsonResponse {
-    $job = Job::with('company')->findOrFail($id);
-    return response()->json($job);
-  }
+    public function index() {
+        try {
+            return $this->jobService->index();
+        } catch (Throwable $e) {
+            return ExceptionHelper::handleException($e);
+        }
+    }
 
-  /**
-   * Update the specified resource in storage.
-   */
-  public function update(Request $request, string $job_id) {
-  }
+    public function store(StoreJobRequest $request) {
+        try {
+            $validatedRequest = $request->validated();
+            $this->jobService->createJob($validatedRequest);
+            return response()->json(['message' => 'Company created successfully']);
+        } catch (Throwable $e) {
+            return ExceptionHelper::handleException($e);
+        }
+    }
 
-  /**
-   * Remove the specified resource from storage.
-   */
-  public function destroy(string $id) {
-    //
-  }
+    public function show(int $jobId): JsonResponse {
+        try {
+            $job = $this->jobService->showJob($jobId);
+            return response()->json($job);
+        } catch (Throwable $e) {
+            return ExceptionHelper::handleException($e);
+        }
+    }
 
-  public function updateJobSkills(Request $request, $job_id) {
-    $job = Job::find($job_id);
-    $skills = $request->input('skills');
+    public function update(UpdateJobRequest $request, int $jobId) {
+        try {
+            $job = Job::findOrFail($jobId);
+            $this->authorize("update", $job);
+            $validatedRequest = $request->validated();
+            $this->jobService->updateJob($validatedRequest, $job);
+            return response()->json(["message" => "Updated job successfully"]);
+        } catch (Throwable $e) {
+            return ExceptionHelper::handleException($e);
+        }
+    }
 
-    $job->skills()->sync($skills);
+    public function destroy(int $jobId) {
+        try {
+            $job = Job::findOrFail($jobId);
+            $this->authorize('delete', $job);
+            $this->jobService->deleteJob($job);
+            return response()->json(["message" => "Job deleted successfully"]);
+        } catch (Throwable $e) {
+            return ExceptionHelper::handleException($e);
+        }
+    }
 
-    return response()->json([
-      "message" => "Job skills updated successfully."
-    ]);
-  }
+    public function getJobs() {
+        try {
+            return $this->jobService->getPaginatedJobsWithDetails();
+        } catch (Throwable $e) {
+            return ExceptionHelper::handleException($e);
+        }
+    }
+
+    public function searchJobs(SearchJobRequest $request) {
+        try {
+            $validatedData = $request->validated();
+            $keyword = $validatedData['keyword'];
+            return $this->jobService->searchJobs($keyword);
+        } catch (Throwable $e) {
+            return ExceptionHelper::handleException($e);
+        }
+    }
+
+    public function updateJobSkills(UpdateJobSkillRequest $request, int $jobId) {
+        try {
+            $job = Job::findOrFail($jobId);
+            $this->authorize('updateJobSkill', $job);
+            $skills = $request->validated()['skills'];
+            $this->jobService->updateJobSkills($skills, $job);
+            return response()->json([
+                "message" => "Job skills updated successfully."
+            ]);
+        } catch (Throwable $e) {
+            return ExceptionHelper::handleException($e);
+        }
+    }
 }
