9d22c33803ebbc83bdc8ace31870e9509bdf827a
Jin <genesisjinsaquibal@gmail.com>
2023-07-11 13:09:12 +0800

    Commit Title: created UpdatedCompany Request

--- app/Http/Requests/UpdateCompanyRequest.php
+++ app/Http/Requests/UpdateCompanyRequest.php
@@ -4,14 +4,12 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class UpdateCompanyRequest extends FormRequest
-{
+class UpdateCompanyRequest extends FormRequest {
     /**
      * Determine if the user is authorized to make this request.
      */
-    public function authorize(): bool
-    {
-        return false;
+    public function authorize(): bool {
+        return true;
     }
 
     /**
@@ -19,10 +17,13 @@ class UpdateCompanyRequest extends FormRequest
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
      */
-    public function rules(): array
-    {
+    public function rules(): array {
         return [
-            //
+            'name' => 'string',
+            'website'  => 'string',
+            'location'  => 'string',
+            'description'  => 'string',
+            'industry' => 'string',
         ];
     }
 }
