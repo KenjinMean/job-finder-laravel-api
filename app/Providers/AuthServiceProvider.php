2676b4b5409e229fc5d6225b168a6505a117b0f7
Jin <genesisjinsaquibal@gmail.com>
2023-07-11 13:06:11 +0800

    Commit Title: removed policies because laravel automatically detects it

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -14,9 +14,7 @@ class AuthServiceProvider extends ServiceProvider {
      *
      * @var array<class-string, class-string>
      */
-    protected $policies = [
-        Job::class => JobSkillPolicy::class,
-    ];
+    protected $policies = [];
 
     /**
      * Register any authentication / authorization services.
