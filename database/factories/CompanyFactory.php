e7c63901580604cce57482a6ba3efadc8f40295c
Jin <genesisjinsaquibal@gmail.com>
2023-07-11 21:27:58 +0800

    modified CompanyFactory to insert random user_id from users

--- database/factories/CompanyFactory.php
+++ database/factories/CompanyFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -16,11 +17,14 @@ class CompanyFactory extends Factory {
     public function definition(): array {
         return [
             'name' => fake()->company,
-            'company_logo' => fake()->image(storage_path('app/public/company_logos'), 200, 200, null, false),
+            'company_logo' => "storage/company_logos/" . fake()->image(storage_path('app/public/company_logos'), 200, 200, null, false),
             'location' => fake()->city,
             'website' => fake()->url,
             'description' => fake()->paragraph,
             'industry' => fake()->jobTitle,
+            'user_id' => function () {
+                return User::inRandomOrder()->first()->id;
+            },
         ];
     }
 }
