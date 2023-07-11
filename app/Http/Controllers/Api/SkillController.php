6c297b15eac8485df6f70ad5152847cab490d489
Jin <genesisjinsaquibal@gmail.com>
2023-07-11 21:19:49 +0800

    impement and refactored SkillController impemented SkillServeice refactored to impement ExceptionHelper implemented show, update, destroy, searchskill methods

--- app/Http/Controllers/Api/SkillController.php
+++ app/Http/Controllers/Api/SkillController.php
@@ -3,47 +3,83 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\Skill;
+use App\Services\SkillService;
+use App\Helpers\ExceptionHelper;
 use App\Http\Controllers\Controller;
-use App\Http\Resources\SkillResource;
+use App\Http\Requests\SearchSkillRequest;
 use App\Http\Requests\StoreSkillRequest;
 use App\Http\Requests\UpdateSkillRequest;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpFoundation\Response;
 
 class SkillController extends Controller {
-    /**
-     * Display a listing of the resource.
-     */
+
+    private $skillService;
+
+    public function __construct(SkillService $skillService) {
+        $this->skillService = $skillService;
+    }
+
     public function index() {
-        $skills = Skill::all();
-        return SkillResource::collection($skills);
+        try {
+            $this->authorize('viewAll');
+            return $this->skillService->getSkills();
+        } catch (\Throwable $e) {
+            return ExceptionHelper::handleException($e);
+        }
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(StoreSkillRequest $request) {
-        $validatedSkill = $request->validated();
-        Skill::create($validatedSkill);
-        return response()->json(['message' => "skill created successfully"]);
+        try {
+            $validatedRequest = $request->validated();
+            $this->authorize('create');
+            $this->skillService->createSkill($validatedRequest);
+            return response()->json(['message' => "Skill created successfully"]);
+        } catch (\Throwable $e) {
+            return ExceptionHelper::handleException($e);
+        }
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Skill $skill) {
-        //
+    public function show($skillId) {
+        try {
+            $skill =  $this->skillService->showSkill($skillId);
+            return response()->json($skill);
+        } catch (\Throwable $e) {
+            return ExceptionHelper::handleException($e);
+        }
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateSkillRequest $request, Skill $skill) {
-        //
+    public function update(UpdateSkillRequest $request, $skillId) {
+        try {
+            $skill = Skill::findOrFail($skillId);
+            $validatedRequest = $request->validated();
+            $this->authorize('update', $skill);
+            $this->skillService->updateSkill($skill, $validatedRequest);
+            return response()->json(["message" => "Skill updated successfully"]);
+        } catch (\Throwable $e) {
+            return ExceptionHelper::handleException($e);
+        }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Skill $skill) {
-        //
+    public function destroy($skillId) {
+        try {
+            $skill = Skill::findOrFail($skillId);
+            $this->authorize('delete', $skill);
+            $this->skillService->deleteSkill($skill);
+            return response()->json(["message" => "Skill deleted successfully"]);
+        } catch (\Throwable $e) {
+            return ExceptionHelper::handleException($e);
+        }
+    }
+
+    public function searchSkill(SearchSkillRequest $request) {
+        try {
+            $validatedData = $request->validated();
+            $keyword = $validatedData['keyword'];
+            $skill = $this->skillService->searchSkill($keyword);
+            return response()->json(["skills" => $skill]);
+        } catch (\Throwable $e) {
+            return ExceptionHelper::handleException($e);
+        }
     }
 }
