6682ca660e1ff627bad6af08e9dc4160863c6f9e
Jin <genesisjinsaquibal@gmail.com>
2023-07-12 13:12:23 +0800

    refactored UserController to use UserService and ExceptionHelper class

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -4,54 +4,81 @@ namespace App\Http\Controllers\Api;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Services\UserService;
+use App\Helpers\ExceptionHelper;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\RegisterUserRequest;
+use App\Http\Requests\UpdateUserRequest;
+use App\Http\Requests\UpdateUserSkillRequest;
 
 class UserController extends Controller {
-    /**
-     * Display a listing of the resource.
-     */
+
+    private $userService;
+
+    public function __construct(UserService $userService) {
+        $this->userService = $userService;
+    }
+
     public function index() {
-        return response(User::all());
+        // return response(User::all());
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(RegisterUserRequest $request) {
-        $validatedData = $request->validated();
-        $user = User::create($validatedData);
-        return response()->json(['message' => $user]);
+        try {
+            $validatedRequest = $request->validated();
+            $user = $this->userService->createUser($validatedRequest);
+            return response()->json(['message' => $user]);
+        } catch (\Throwable $e) {
+            return ExceptionHelper::handleException($e);
+        }
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show() {
-        return response(Auth::user());
+        try {
+            $user = Auth::user();
+            $this->authorize('view', $user);
+            $response = $this->userService->getUser();
+            return response()->json(["user" => $response]);
+        } catch (\Throwable $e) {
+            return ExceptionHelper::handleException($e);
+        }
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id) {
-        //
+    public function update(UpdateUserRequest $request) {
+        try {
+            $user = Auth::user();
+            $validatedRequest = $request->validated();
+            $this->authorize('update', $user);
+            $this->userService->updateUser($user, $validatedRequest);
+            return response()->json(['message' => 'Updated successfully']);
+        } catch (\Throwable $e) {
+            return ExceptionHelper::handleException($e);
+        }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id) {
-        //
+        try {
+            $user = User::findOrFail($id);
+            $this->authorize('delete', $user);
+            $this->userService->deleteUser($user);
+            return response()->json(['message' => 'User deleted successfully']);
+        } catch (\Throwable $e) {
+            return ExceptionHelper::handleException($e);
+        }
     }
 
-    public function updateSkill(Request $request) {
-        $user = Auth::user();
-        /** @var User $user */
-        $user->skills()->sync($request->input('skills'));
-        return response()->json([
-            "message" => "Skills updated successfully."
-        ]);
+    public function updateSkill(UpdateUserSkillRequest $request) {
+        try {
+            $user = Auth::user();
+            $skills = $request->validated()['skills'];
+            $this->authorize('update', $user);
+            $this->userService->updateSkill($user, $skills);
+            return response()->json([
+                "message" => "Skills updated successfully."
+            ]);
+        } catch (\Throwable $e) {
+            return ExceptionHelper::handleException($e);
+        }
     }
 }
