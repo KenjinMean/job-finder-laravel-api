ec449f7cd6fa6a60dee41f8988c038c95f61c84a
Jin <genesisjinsaquibal@gmail.com>
2023-07-21 18:14:58 +0800

    implmented MustVerifyEmail, JWTsubject and it's methods

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,12 @@ use App\Models\Skill;
 use App\Models\Company;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable {
+class User extends Authenticatable implements MustVerifyEmail, JWTSubject {
     use HasApiTokens, HasFactory, Notifiable;
 
     /**
@@ -43,6 +45,24 @@ class User extends Authenticatable {
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier() {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims() {
+        return [];
+    }
 
     public function companies() {
         return $this->hasMany(Company::class);
