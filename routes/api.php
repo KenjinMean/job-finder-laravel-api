8cd2b77d17b5a21f8f38abde4cd9edb22bd632df
Jin <genesisjinsaquibal@gmail.com>
2023-07-21 10:21:52 +0800

    created email verification routes

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,15 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\JobController;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\UserController;
-use App\Http\Controllers\Api\CompanyController;
 use App\Http\Controllers\Api\SkillController;
+use App\Http\Controllers\Api\CompanyController;
 use App\Http\Controllers\Api\UserInfoController;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,8 +25,34 @@ use App\Http\Controllers\Api\UserInfoController;
 Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
 
+// Custom Email Verification Route...
+Route::get('/email/verify', function (Request $request) {
+    $user = $request->user();
+    // Check if the user is authenticated and the email is verified
+    if ($user && $user->hasVerifiedEmail()) {
+        return response()->json(['message' => 'Email already verified.'], 200);
+    }
+    return response()->json(['message' => 'Email verification required.'], 403);
+})->middleware(['auth:api', 'throttle:6,1'])->name('verification.notice');
 
-Route::middleware('auth:sanctum')->group(function () {
+Route::get('/email/verify/{id}/{hash}', function (Request $request, $id, $hash) {
+    $user = User::findOrFail($id);
+    if (!hash_equals((string) $hash, sha1($user->getEmailForVerification()))) {
+        return response()->json(['message' => 'Invalid verification link.'], 403);
+    }
+    if (!$user->hasVerifiedEmail()) {
+        $user->markEmailAsVerified();
+    }
+    return response()->json(['message' => 'Email verified successfully.']);
+})->name('verification.verify');
+
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+    return response()->json(['message' => 'Verification link sent!']);
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
+
+// Route::middleware('auth:api')->group(function () {
+Route::middleware('auth:api', 'verified')->group(function () {
 
     // Route::get('skills', [SkillController::class, 'index']);
 
@@ -62,5 +91,5 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/jobs/search-jobs', [JobController::class, 'searchJobs']);
     Route::apiResource('/jobs', JobController::class);
 
-    Route::post('logout', [AuthController::class, 'logout']);
+    // Route::post('logout', [AuthController::class, 'logout']);
 });
