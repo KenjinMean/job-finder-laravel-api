042cdea21a27473ca41490ae8e3f32f32a09cc23
Jin <genesisjinsaquibal@gmail.com>
2023-07-22 21:45:33 +0800

    refactored routes, determine if routes needed to be public or protected

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\SkillController;
 use App\Http\Controllers\Api\CompanyController;
+use App\Http\Controllers\Api\EmailVerificationController;
+use App\Http\Controllers\Api\SocialAuthController;
 use App\Http\Controllers\Api\UserInfoController;
 
 /*
@@ -19,30 +21,61 @@ use App\Http\Controllers\Api\UserInfoController;
 |
 */
 
-Route::post('login', [AuthController::class, 'login']);
-Route::post('register', [AuthController::class, 'register']);
+# Authentication Routes
+Route::post('login', [AuthController::class, 'login'])->name('login');
+Route::post('register', [AuthController::class, 'register'])->name('register');
+Route::post('logout', [AuthController::class, 'logout'])
+    ->middleware(['auth:api', 'verified', 'check.token.expiration'])
+    ->name('logout');
+
+# OAuth service providers route :: SOCIALITE
+Route::get('/auth/{provider}/redirect', [SocialAuthController::class, 'redirectToProvider'])->name('social.redirect');
+Route::get('/auth/{provider}/callback', [SocialAuthController::class, 'handleProviderCallback'])->name('social.callback');
 
 # Custom Email Verification Route
 Route::prefix('email')->group(function () {
-    Route::get('/verify', [AuthController::class, 'verificationNotice'])
+    Route::get('/verify', [EmailVerificationController::class, 'verificationNotice'])
         ->middleware(['auth:api', 'throttle:6,1'])
         ->name('verification.notice');
-    Route::get('/verify/{id}/{hash}', [AuthController::class, 'verificationVerify'])
+    Route::get('/verify/{id}/{hash}', [EmailVerificationController::class, 'verificationVerify'])
         ->name('verification.verify');
-    Route::post('/verification-notification', [AuthController::class, 'verificationSend'])
+    Route::post('/verification-notification', [EmailVerificationController::class, 'verificationSend'])
         ->middleware(['auth:api', 'throttle:6,1'])
         ->name('verification.send');
-    Route::get('/verification-redirect', [AuthController::class, 'verificationRedirect']);
+    Route::get('/verification-redirect', [EmailVerificationController::class, 'verificationRedirect']);
 });
 
-# NOTE make some routes accessible, auth, then verified
+# DEVELOPER ROUTES
+Route::get('skills', [SkillController::class, 'index']);
+Route::apiResource('skills', SkillController::class);
 
-# Protected Routes
-// Route::middleware('auth:api')->group(function () {
-Route::middleware('auth:api', 'verified')->group(function () {
-    // Route::get('skills', [SkillController::class, 'index']);
+# JOB ROUTES
+Route::get('/jobs/get-jobs', [JobController::class, 'getJobs']);
+Route::get('/jobs/search-jobs', [JobController::class, 'searchJobs']);
+Route::get('/jobs/{job}', [JobController::class, 'show'])->name('job.show');
+Route::middleware(['auth:api', 'verified', 'check.token.expiration'])->group(function () {
+    Route::post('/jobs', [JobController::class, 'store'])->name('job.store');
+    Route::patch('/jobs/update-job-skills/{job}', [JobController::class, 'updateJobSkills']);
+    Route::patch('/jobs/update-skills', [JobController::class, 'updateSkill'])->name('job.update-skill');
+    Route::patch('/jobs/{job}', [JobController::class, 'update'])->name('job.update');
+    Route::delete('/jobs/{job}', [JobController::class, 'destroy'])->name('job.destroy');
+    // Route::apiResource('/jobs', JobController::class);
+});
 
-    # User routes
+# COMPANY ROUTES
+// Route::apiResource('companies', CompanyController::class);
+Route::get('/companies/{company}', [CompanyController::class, 'show'])->name('company.show');
+Route::middleware(['auth:api', 'verified', 'check.token.expiration'])->group(function () {
+    Route::get('companies/user-companies', [CompanyController::class, 'showUserCompanies'])->name('companies.user-companies');
+    Route::patch('companies/update-company-image/{company}', [CompanyController::class, 'updateCompanyImage'])->name('companies.update-company-image');
+    Route::post('/companies', [CompanyController::class, 'store'])->name('company.store');
+    Route::patch('/companies/{company}', [CompanyController::class, 'update'])->name('company.update');
+    Route::delete('/companies/{company}', [CompanyController::class, 'destroy'])->name('company.destroy');
+});
+
+# PROTECTED GROUPED ROUTES
+Route::middleware(['auth:api', 'verified', 'check.token.expiration'])->group(function () {
+    # USER ROUTES
     Route::prefix('users')->group(function () {
         Route::post('/store', [UserController::class, 'store'])->name('user.store');
         Route::get('/show', [UserController::class, 'show'])->name('user.show');
@@ -51,11 +84,10 @@ Route::middleware('auth:api', 'verified')->group(function () {
         Route::patch('/update-skills', [UserController::class, 'updateSkill'])->name('user.update-skill');
     });
 
-    #Skill routes
+    # SKILL ROUTES
     Route::get('search-skills', [SkillController::class, 'searchSkill'])->name('skills.search-skill');
-    Route::apiResource('skills', SkillController::class);
 
-    # User Info routes
+    # USER INFO ROUTES
     Route::prefix('user-infos')->group(function () {
         Route::get('/', [UserInfoController::class, 'index'])->name('user-infos.index');
         Route::post('/store', [UserInfoController::class, 'store'])->name('user-infos.store');
@@ -65,17 +97,4 @@ Route::middleware('auth:api', 'verified')->group(function () {
         Route::patch('/update-profile-image', [UserInfoController::class, 'updateProfileImage'])->name('user-infos.update-profile');
         Route::patch('/update-cover-image', [UserInfoController::class, 'updateCoverImage'])->name('user-infos.update-cover');
     });
-
-    # Company routes
-    Route::get('companies/user-companies', [CompanyController::class, 'showUserCompanies'])->name('companies.user-companies');
-    Route::patch('companies/update-company-image/{company}', [CompanyController::class, 'updateCompanyImage'])->name('companies.update-company-image');
-    Route::apiResource('companies', CompanyController::class);
-
-    # Job routes
-    Route::get('/jobs/get-jobs', [JobController::class, 'getJobs']);
-    Route::patch('/jobs/update-job-skills/{job}', [JobController::class, 'updateJobSkills']);
-    Route::get('/jobs/search-jobs', [JobController::class, 'searchJobs']);
-    Route::apiResource('/jobs', JobController::class);
-
-    Route::post('logout', [AuthController::class, 'logout']);
 });
