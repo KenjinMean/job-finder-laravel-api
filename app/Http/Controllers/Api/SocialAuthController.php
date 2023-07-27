e5fcd54a09f4c8643a5567ea78d1ad61acad4186
Jin <genesisjinsaquibal@gmail.com>
2023-07-27 14:30:37 +0800

    implemented getProviderAuthorizationUrl

--- app/Http/Controllers/Api/SocialAuthController.php
+++ app/Http/Controllers/Api/SocialAuthController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers\Api;
 
-use Illuminate\Http\Request;
+use Throwable;
+use Illuminate\Http\Response;
+use App\Helpers\ResponseHelper;
+use App\Helpers\ExceptionHelper;
 use App\Services\SocialAuthService;
 use App\Http\Controllers\Controller;
-use Laravel\Socialite\Facades\Socialite;
 
 class SocialAuthController extends Controller {
     private $socialAuthService;
@@ -13,17 +15,33 @@ class SocialAuthController extends Controller {
     public function __construct(SocialAuthService $socialAuthService) {
         $this->socialAuthService = $socialAuthService;
     }
+
     # Socialite methods
+    public function getProviderAuthorizationUrl($provider) {
+        try {
+            $authorizationUrl = $this->socialAuthService->getProviderAuthorizationUrl($provider);
+            return response()->json(['authorization_url' => $authorizationUrl]);
+        } catch (\Exception $e) {
+            return ResponseHelper::errorResponse('Failed to get ' . $provider . ' authorization URL', Response::HTTP_INTERNAL_SERVER_ERROR, $e->getMessage());
+        }
+    }
+
     public function redirectToProvider($provider) {
-        return $this->socialAuthService->redirectToProvider($provider);
+        try {
+            return $this->socialAuthService->redirectToProvider($provider);
+        } catch (Throwable $e) {
+            return ExceptionHelper::handleException($e);
+        }
     }
 
     public function handleProviderCallback($provider) {
         try {
-            $user = $this->socialAuthService->handleProviderCallback($provider);
-            return response()->json(['user' => $user]);
+            // return redirect('http://localhost:3000/callback?token=' . $token);
+            // return redirect(env('FRONTEND_URL') . '/callback?token=' . $token);
+            $redirectUrl = $this->socialAuthService->handleProviderCallback($provider);
+            return $redirectUrl;
         } catch (\Exception $e) {
-            return response()->json(['error' => 'Failed to authenticate with ' . $provider]);
+            return ResponseHelper::errorResponse('Failed to authenticate with ' . $provider, Response::HTTP_INTERNAL_SERVER_ERROR, $e->getMessage());
         }
     }
 }
