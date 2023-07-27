ff616ee23fdcce5812702a0e2be74d2b3947f1a3
Jin <genesisjinsaquibal@gmail.com>
2023-07-27 14:39:11 +0800

    fixed middleware orders

--- routes/api.php
+++ routes/api.php
@@ -24,13 +24,15 @@ use App\Http\Controllers\Api\UserInfoController;
 # Authentication Routes
 Route::post('login', [AuthController::class, 'login'])->name('login');
 Route::post('register', [AuthController::class, 'register'])->name('register');
-Route::post('logout', [AuthController::class, 'logout'])
-    ->middleware(['auth:api', 'check.token.expiration'])
-    ->name('logout');
+Route::middleware(['check.token.expiration', 'auth:api'])->group(function () {
+    Route::post('logout', [AuthController::class, 'logout'])
+        ->name('logout');
+});
 
 # OAuth service providers route :: SOCIALITE
 Route::get('/auth/{provider}/redirect', [SocialAuthController::class, 'redirectToProvider'])->name('social.redirect');
 Route::get('/auth/{provider}/callback', [SocialAuthController::class, 'handleProviderCallback'])->name('social.callback');
+Route::get('/auth/{provider}/get-authorization-url', [SocialAuthController::class, 'getProviderAuthorizationUrl'])->name('social.callback');
 
 # Custom Email Verification Route
 Route::prefix('email')->group(function () {
@@ -45,43 +47,43 @@ Route::prefix('email')->group(function () {
     Route::get('/verification-redirect', [EmailVerificationController::class, 'verificationRedirect']);
 });
 
-# DEVELOPER ROUTES
+# SKILL ROUTES
 Route::get('skills', [SkillController::class, 'index']);
 Route::apiResource('skills', SkillController::class);
 
 # JOB ROUTES
-Route::get('/jobs/get-jobs', [JobController::class, 'getJobs']);
-Route::get('/jobs/search-jobs', [JobController::class, 'searchJobs']);
+Route::get('/jobs/get-jobs', [JobController::class, 'getJobPostings'])->name('job.get-posting');
+Route::get('/jobs/search-jobs', [JobController::class, 'searchJobs'])->name('job.search');
 Route::get('/jobs/{job}', [JobController::class, 'show'])->name('job.show');
-Route::middleware(['auth:api', 'verified', 'check.token.expiration'])->group(function () {
+Route::middleware(['check.token.expiration', 'auth:api', 'verified'])->group(function () {
     Route::post('/jobs', [JobController::class, 'store'])->name('job.store');
-    Route::patch('/jobs/update-job-skills/{job}', [JobController::class, 'updateJobSkills']);
-    Route::patch('/jobs/update-skills', [JobController::class, 'updateSkill'])->name('job.update-skill');
+    Route::patch('/jobs/update-job-skills/{job}', [JobController::class, 'updateJobSkills'])->name('job.update-job-skill');
     Route::patch('/jobs/{job}', [JobController::class, 'update'])->name('job.update');
     Route::delete('/jobs/{job}', [JobController::class, 'destroy'])->name('job.destroy');
     // Route::apiResource('/jobs', JobController::class);
 });
 
 # COMPANY ROUTES
-// Route::apiResource('companies', CompanyController::class);
-Route::get('/companies/{company}', [CompanyController::class, 'show'])->name('company.show');
-Route::middleware(['auth:api', 'verified', 'check.token.expiration'])->group(function () {
-    Route::get('/companies', [CompanyController::class, 'index'])->name('companies.index');
+Route::middleware(['check.token.expiration', 'auth:api', 'verified'])->group(function () {
     Route::get('companies/user-companies', [CompanyController::class, 'showUserCompanies'])->name('companies.user-companies');
-    Route::patch('companies/update-company-image/{company}', [CompanyController::class, 'updateCompanyImage'])->name('companies.update-company-image');
+    Route::get('/companies', [CompanyController::class, 'index'])->name('companies.index');
     Route::post('/companies', [CompanyController::class, 'store'])->name('company.store');
     Route::patch('/companies/{company}', [CompanyController::class, 'update'])->name('company.update');
     Route::delete('/companies/{company}', [CompanyController::class, 'destroy'])->name('company.destroy');
+    Route::patch('companies/update-company-image/{company}', [CompanyController::class, 'updateCompanyImage'])->name('companies.update-company-image');
 });
+Route::get('/companies/{company}', [CompanyController::class, 'show'])->name('company.show');
+// Route::apiResource('companies', CompanyController::class);
 
 # PROTECTED GROUPED ROUTES
-Route::middleware(['auth:api', 'verified', 'check.token.expiration'])->group(function () {
+Route::delete('users/destroy', [UserController::class, 'destroy'])->name('user.destroy');
+Route::middleware(['check.token.expiration', 'auth:api', 'verified'])->group(function () {
     # USER ROUTES
     Route::prefix('users')->group(function () {
         Route::post('/store', [UserController::class, 'store'])->name('user.store');
         Route::get('/show', [UserController::class, 'show'])->name('user.show');
         Route::patch('/update', [UserController::class, 'update'])->name('user.update');
-        Route::delete('/destroy', [UserController::class, 'destroy'])->name('user.destroy');
+
         Route::patch('/update-skills', [UserController::class, 'updateSkill'])->name('user.update-skill');
     });
 
@@ -90,7 +92,7 @@ Route::middleware(['auth:api', 'verified', 'check.token.expiration'])->group(fun
 
     # USER INFO ROUTES
     Route::prefix('user-infos')->group(function () {
-        Route::get('/', [UserInfoController::class, 'index'])->name('user-infos.index');
+        // Route::get('/', [UserInfoController::class, 'index'])->name('user-infos.index');
         Route::post('/store', [UserInfoController::class, 'store'])->name('user-infos.store');
         Route::get('/show', [UserInfoController::class, 'show'])->name('user-infos.show');
         Route::put('/update', [UserInfoController::class, 'update'])->name('user-infos.update');
