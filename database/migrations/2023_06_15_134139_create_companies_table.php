1580a0889a2f087583f013eac56fd64cf7ccd1ed
Jin <genesisjinsaquibal@gmail.com>
2023-08-22 22:26:45 +0800

    add cascade on delete on foreign field

--- database/migrations/2023_06_15_134139_create_companies_table.php
+++ database/migrations/2023_06_15_134139_create_companies_table.php
@@ -19,7 +19,7 @@ return new class extends Migration {
             $table->string('industry');
             $table->timestamps();
             $table->unsignedBigInteger('user_id')->nullable();
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
