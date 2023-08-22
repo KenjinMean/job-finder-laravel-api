6cd43209d5a6de652d6c8a6a48c46a745bfcc6d6
Jin <genesisjinsaquibal@gmail.com>
2023-08-22 21:02:30 +0800

    created helper functions for jwt token

--- app/Helpers/JwtHelper.php
+++ app/Helpers/JwtHelper.php
@@ -3,6 +3,9 @@
 
 namespace App\Helpers;
 
+use App\Models\User;
+use App\Http\Resources\UserResource;
+use App\Services\RefreshTokenGenerator;
 use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
 
 class JwtHelper {
@@ -11,8 +14,47 @@ class JwtHelper {
       $token = JWTAuth::getToken();
       return JWTAuth::toUser($token);
     } catch (\Throwable $e) {
-      // Handle any exceptions that might occur during the process
       return null;
     }
+  }
+
+  public static function generateAccessToken($user) {
+    $token = JWTAuth::fromUser($user);
+    $user->load('userInfo');
+    $userResource = new UserResource($user);
+    $expiresInMinutes = JWTAuth::factory()->getTTL() * 60;
+
+    return response()->json([
+      'user' => $userResource,
+      'access_token' => $token,
+      'token_type' => 'bearer',
+      'expires_in' => $expiresInMinutes,
+    ]);
+  }
+
+  public static function refreshAccessToken() {
+    $token = JWTAuth::parseToken()->refresh();
+    $expiresInMinutes = JWTAuth::factory()->getTTL() * 60;
+
+    return response()->json([
+      'access_token' => $token,
+      'expires_in' => $expiresInMinutes,
+    ]);
+  }
+
+  public static function generateAccessTokenAndSetRefreshCookie(User $user) {
+    $token = JWTAuth::fromUser($user);
+    $refreshToken = RefreshTokenGenerator::generateRefreshToken($user);
+    $user->load('userInfo');
+    $userResource = new UserResource($user);
+    $expiresInMinutes = 30 * 24 * 60; // 30 days
+
+    return response()->json([
+      'user' => $userResource,
+      'access_token' => $token,
+      'token_type' => 'bearer',
+      'expires_in' => JWTAuth::factory()->getTTL() * 60,
+    ])
+      ->withCookie(cookie('refresh_token', $refreshToken, $expiresInMinutes));
   }
 }
