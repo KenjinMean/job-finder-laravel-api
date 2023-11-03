bddef65b39b53c98abe3270ebd1d5cb40bd6e2eb
Jin <genesisjinsaquibal@gmail.com>
2023-11-03 11:14:53 +0800

    finalize migrations fileds

---
+++ database/migrations/2023_06_14_134140_create_companies_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->string('name');
+            $table->string('location')->nullable();
+            $table->string('company_logo')->nullable();
+            $table->string('website')->nullable();
+            $table->text('description')->nullable();
+            $table->string('industry')->nullable();
+            $table->text('benefits')->nullable();
+            $table->string('email')->nullable();
+            $table->string('phone')->nullable();
+            $table->text('mission')->nullable();
+            $table->text('vision')->nullable();
+            $table->unsignedBigInteger('company_size_id')->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('company_size_id')->references('id')->on('company_size_categories')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void {
+        Schema::dropIfExists('companies');
+    }
+};
--- database/migrations/2023_06_15_133656_create_jobs_table.php
+++ database/migrations/2023_06_15_133656_create_jobs_table.php
@@ -11,6 +11,43 @@ return new class extends Migration {
     public function up(): void {
         Schema::create('jobs', function (Blueprint $table) {
             $table->id();
+            $table->string('slug')->unique();
+            $table->string('title', 255)->index();
+            $table->string('location', 255)->nullable()->index();
+            $table->text('description')->nullable();
+            $table->text('requirements')->nullable();
+            $table->string('salary', 50)->nullable();
+            $table->text("application_process")->nullable();
+            $table->text('benefits')->nullable();
+            $table->timestamp('application_deadline_at')->nullable();
+            $table->timestamps();
+            $table->unsignedBigInteger('company_id');
+            $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void {
+        Schema::dropIfExists('jobs');
+    }
+};
+
+/** 
+
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    //  Run the migrations.
+      public function up(): void {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->id();
             $table->string('title', 255);
             $table->string('location', 255);
             $table->text('description');
@@ -22,10 +59,11 @@ return new class extends Migration {
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
+    // Reverse the migrations.
+     
     public function down(): void {
         Schema::dropIfExists('jobs');
     }
 };
+
+ */
