54e92be31b3e60dd82a3ef23f72721103bc47690
Jin <genesisjinsaquibal@gmail.com>
2023-11-05 20:57:49 +0800

    title: used range for company size instead of category

--- database/seeders/CompanySizeCategorySeeder.php
+++ database/seeders/CompanySizeCategorySeeder.php
@@ -11,13 +11,13 @@ class CompanySizeCategorySeeder extends Seeder {
      */
     public function run() {
         $categories = [
-            'Micro',
-            'Small',
-            'Medium',
-            'Large',
-            'Enterprise',
-            'Large Enterprise',
-            'Global Corporation',
+            'Below 10',
+            '10-50',
+            '51-200',
+            '201-500',
+            '501-1,000',
+            '1,001-5,000',
+            'Over 5,000',
         ];
 
         foreach ($categories as $category) {
