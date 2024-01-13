d8b2d6210df4fb0650a3bb5b9921faa9751065ae
Jin <genesisjinsaquibal@gmail.com>
2024-01-13 20:46:26 +0800

    feat: implemented userContact api and resources

--- app/Models/UserContact.php
+++ app/Models/UserContact.php
@@ -5,7 +5,25 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class UserContact extends Model
-{
+class UserContact extends Model {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'phone',
+        'city',
+        'province',
+        'country',
+        'zip_code',
+        "birth_date",
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
