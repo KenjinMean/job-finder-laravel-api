a71afeb7e6de8d650e7c24212b7ac8d121067b1d
Jin <genesisjinsaquibal@gmail.com>
2024-02-29 11:23:03 +0800

    Chore: setup user work experience policy

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,9 +4,11 @@ namespace App\Providers;
 
 use App\Models\Job;
 use App\Models\Skill;
+use App\Models\UserWorkExperience;
 use App\Policies\SkillPolicy;
 use App\Policies\CompanyPolicy;
 use App\Policies\JobSkillPolicy;
+use App\Policies\UserWorkExperiencePolicy;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -18,6 +20,7 @@ class AuthServiceProvider extends ServiceProvider {
      */
     protected $policies = [
         Skill::class => SkillPolicy::class,
+        UserWorkExperience::class => UserWorkExperiencePolicy::class,
     ];
 
     /**
