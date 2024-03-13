644b8b09babb66331f04ee775dddab1180b8e1b3
Jin <genesisjinsaquibal@gmail.com>
2024-03-13 10:28:21 +0800

    Chore: moved user skill functions to user controller

--- app/Http/Controllers/Api/SkillController.php
+++ app/Http/Controllers/Api/SkillController.php
@@ -3,16 +3,11 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\Skill;
-use App\Helpers\JwtHelper;
 use App\Services\SkillService;
-use App\Helpers\ExceptionHelper;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\AddSkillRequest;
 use App\Http\Requests\StoreSkillRequest;
 use App\Http\Requests\SearchSkillRequest;
 use App\Http\Requests\UpdateSkillRequest;
-use Illuminate\Validation\ValidationException;
-use Symfony\Component\HttpFoundation\Response;
 
 class SkillController extends Controller {
 
@@ -23,94 +18,40 @@ class SkillController extends Controller {
     }
 
     public function index() {
-        try {
-            $this->authorize('viewAll', Skill::class);
-            return $this->skillService->getSkills();
-        } catch (\Throwable $e) {
-            return ExceptionHelper::handleException($e);
-        }
+        $this->authorize('viewAll', Skill::class);
+        return $this->skillService->getSkills();
     }
 
     public function store(StoreSkillRequest $request) {
-        try {
-            $validatedRequest = $request->validated();
-            $this->authorize('create');
-            $this->skillService->createSkill($validatedRequest);
-            return response()->json(['message' => "Skill created successfully"]);
-        } catch (\Throwable $e) {
-            return ExceptionHelper::handleException($e);
-        }
+        $validatedRequest = $request->validated();
+        $this->authorize('create');
+        $this->skillService->createSkill($validatedRequest);
+        return response()->json(['message' => "Skill created successfully"]);
     }
 
-    public function show($skillId) {
-        try {
-            $skill =  $this->skillService->showSkill($skillId);
-            return response()->json($skill);
-        } catch (\Throwable $e) {
-            return ExceptionHelper::handleException($e);
-        }
+    public function show(Skill $skill) {
+        $this->authorize('viewAll', Skill::class);
+        return response()->json($skill);
     }
 
-    // public function update(UpdateSkillRequest $request, $skillId) {
-    //     try {
-    //         $skill = Skill::findOrFail($skillId);
-    //         $validatedRequest = $request->validated();
-    //         $this->authorize('update', $skill);
-    //         $this->skillService->updateSkill($skill, $validatedRequest);
-    //         return response()->json(["message" => "Skill updated successfully"]);
-    //     } catch (\Throwable $e) {
-    //         return ExceptionHelper::handleException($e);
-    //     }
-    // }
+    public function update(UpdateSkillRequest $request, $skillId) {
+        $skill = Skill::findOrFail($skillId);
+        $validatedRequest = $request->validated();
+        $this->authorize('update', $skill);
+        $this->skillService->updateSkill($skill, $validatedRequest);
+        return response()->json(["message" => "Skill updated successfully"]);
+    }
 
     public function destroy($skillId) {
-        try {
-            $skill = Skill::findOrFail($skillId);
-            $this->authorize('delete', $skill);
-            $this->skillService->deleteSkill($skill);
-            return response()->json(["message" => "Skill deleted successfully"]);
-        } catch (\Throwable $e) {
-            return ExceptionHelper::handleException($e);
-        }
-    }
-
-    public function getUserSkills() {
-        try {
-            $user = JwtHelper::getUserFromToken();
-            $skill = $this->skillService->getUserSkills($user);
-            return response()->json(["skills" => $skill]);
-        } catch (\Throwable $e) {
-            return ExceptionHelper::handleException($e);
-        }
+        $skill = Skill::findOrFail($skillId);
+        $this->authorize('delete', $skill);
+        $this->skillService->deleteSkill($skill);
+        return response()->json(["message" => "Skill deleted successfully"]);
     }
 
     public function searchSkill(SearchSkillRequest $request) {
-        try {
-            $keyword = $request->validated()['keyword'];
-            $skill = $this->skillService->searchSkill($keyword);
-            return response()->json(["skills" => $skill]);
-        } catch (\Throwable $e) {
-            return ExceptionHelper::handleException($e);
-        }
-    }
-
-    public function addSkill(AddSkillRequest $request) {
-        try {
-            $user = JwtHelper::getUserFromToken();
-            $skillId = $request->validated()['skill_id'];
-            return $this->skillService->addSkill($user, $skillId);
-        } catch (\Throwable $e) {
-            return ExceptionHelper::handleException($e);
-        }
-    }
-
-    public function removeSkill(AddSkillRequest $request) {
-        try {
-            $user = JwtHelper::getUserFromToken();
-            $skillId = $request->validated()['skill_id'];
-            return $this->skillService->removeSkill($user, $skillId);
-        } catch (\Throwable $e) {
-            return ExceptionHelper::handleException($e);
-        }
+        $keyword = $request->validated()['keyword'];
+        $skill = $this->skillService->searchSkill($keyword);
+        return response()->json(["skills" => $skill]);
     }
 }
--- app/Services/SkillService.php
+++ app/Services/SkillService.php
@@ -11,25 +11,21 @@ class SkillService {
     return SkillResource::collection(Skill::all());
   }
 
-  public function createSkill($validatedRequest): void {
-    Skill::create($validatedRequest);
-  }
-
   public function showSkill($skillId) {
     return Skill::findOrFail($skillId);
   }
 
-  // public function updateSkill($skill, $validatedRequest): void {
-  //   $skill->update($validatedRequest);
-  // }
+  public function createSkill($validatedRequest): void {
+    Skill::create($validatedRequest);
+  }
+
+  public function updateSkill($user, $skillId) {
+    $userId = $user->id;
+    return response()->json(["skill" => $skillId, "user" => $userId]);
+  }
 
   public function deleteSkill($skill): void {
     $skill->delete();
-  }
-
-  public function getUserSkills($user) {
-    $skills = $user->skills;
-    return SkillResource::collection($skills);
   }
 
   public function searchSkill($keyword) {
@@ -43,57 +39,5 @@ class SkillService {
         ->get();
     }
     return SkillResource::collection($skills);
-  }
-
-  public function updateSkill($user, $skillId) {
-    $userId = $user->id;
-    return response()->json(["skill" => $skillId, "user" => $userId]);
-  }
-
-  public function addSkill($user, $skillId) {
-    try {
-
-      if ($user->skills()->where('skill_id', $skillId)->exists()) {
-
-        throw new \Exception('Skill is already associated with the user.');
-      }
-
-      Skill::findOrFail($skillId);
-      $user->skills()->attach($skillId);
-
-      return response()->json(["message" => "Skills added successfully"]);
-    } catch (\Throwable $e) {
-      throw new \Exception($e->getMessage());
-    }
-  }
-
-  public function removeSkill($user, $skillId) {
-    try {
-      Skill::findOrFail($skillId);
-      $user->skills()->detach($skillId);
-
-      return response()->json(["message" => "Skills removed successfully"]);
-    } catch (\Throwable $e) {
-      throw new \Exception($e->getMessage());
-    }
-  }
-
-  // remove skills based on array of skills provided
-  public function removeSkills($user, $skillIds) {
-    try {
-      DB::beginTransaction();
-
-      foreach ($skillIds as $skillId) {
-        Skill::findOrFail($skillId);
-        $user->skills()->detach($skillId);
-      }
-
-      DB::commit();
-
-      return response()->json(["message" => "Skills removed successfully"]);
-    } catch (\Throwable $e) {
-      DB::rollBack();
-      throw new \Exception($e->getMessage());
-    }
   }
 }
