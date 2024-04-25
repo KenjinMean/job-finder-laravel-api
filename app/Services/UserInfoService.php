85a167ac91abe21d0ba7d8e8e04130668dc4ec8c
Jin <genesisjinsaquibal@gmail.com>
2024-04-25 21:12:50 +0800

    Refactor: Refactored UserInfoService for improved readability and maintainability. -Used type hinting and dependency injection: Improved type safety and   clarity by using type hints for method arguments and injecting   dependencies through constructor or method arguments. -Introduced specific methods for CRUD operations: Separated logic for   create, read, update, and delete operations into dedicated methods   for better organization. -Enhanced error handling: Threw specific exceptions like   NotFoundHttpException for clarity and consistency with Laravel's   exception handling practices. -Improved method naming: Renamed methods like updateUserInfo to   update for better conciseness and consistency. -Combined updateProfileImage and updateCoverImage: Refactored the   logic into a single updateImage method with an imageType parameter   for handling both profile and cover image updates. -Extracted image deletion logic: Moved the logic for deleting   existing images to a separate deleteImage method for reusability. -Added Docblocks: Provided documentation comments using Docblocks   to explain the purpose of methods and parameters.

--- app/Services/UserInfoService.php
+++ app/Services/UserInfoService.php
@@ -2,113 +2,101 @@
 
 namespace App\Services;
 
-use Illuminate\Support\Facades\DB;
+use App\Models\User;
+use App\Models\UserInfo;
 use Illuminate\Support\Facades\Storage;
 use App\Http\Resources\UserInfoResource;
+use Symfony\Component\HttpKernel\Exception\ConflictHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class UserInfoService {
-  public function index($user) {
+  public function index(User $user): UserInfoResource {
     $userInfo = $user->userInfo;
 
     if (!$userInfo) {
-      return response()->json(['error' => 'User info not found'], 404);
+      throw new NotFoundHttpException('User info not found');
     }
 
     return new UserInfoResource($userInfo);
   }
 
-  public function store() {
+  // |--------------------------------------------------------------------------
+  public function store(User $user, array $validatedRequest): void {
+    if ($user->userInfo) {
+      throw new ConflictHttpException('User already has a contact');
+    }
+
+    $validatedRequest['user_id'] = $user->id;
+    UserInfo::create($validatedRequest);
   }
 
-  public function updateUserInfo($user, $validatedRequest) {
+
+  // |--------------------------------------------------------------------------
+  public function update(User $user, array $validatedRequest) {
     $userInfo = $user->userInfo;
     $userInfo->fill($validatedRequest)->save();
   }
 
-  # NOTE can refactor this update profile image to update cover image
-  private function deleteOldProfileImage($oldProfileImage) {
-    // Extract the path from the old image URL
-    $oldProfileImage = str_replace('storage/', '', $oldProfileImage);
-
-    // Delete the old image if not default image
-    if ($oldProfileImage !== 'user_profile_images/default-avatar.png') {
-      Storage::disk('public')->delete($oldProfileImage);
+  // |--------------------------------------------------------------------------
+  public function delete(User $user) {
+    if ($user->userInfo) {
+      $user->userInfo->delete();
     }
   }
 
-  public function updateProfileImage($user, $validatedRequest) {
-    try {
-      $path = null;
-      $oldProfileImage = $user->userInfo->profile_image;
-      DB::beginTransaction();
+  // |--------------------------------------------------------------------------
+  /**
+   * Update profile or cover image for the user.
+   *
+   * @param \App\Models\User $user The user for whom the image is being updated.
+   * @param array $validatedRequest The validated request containing the new image data.
+   * @param string $imageType The type of image being updated (profile or cover).
+   * @return void
+   */
+  public function updateImage(User $user, $validatedRequest, string $imageType): void {
+    // Upload the new image
+    $newImage = Storage::disk('public')->put('user_' . $imageType . '_images', $validatedRequest[$imageType . '_image']);
 
-      if (isset($validatedRequest['profile_image'])) {
-        // New image is provided
-        $path = Storage::disk('public')->put('user_profile_images', $validatedRequest['profile_image']);
-        $path = 'storage/' . str_replace('\\', '/', $path);
-        $this->deleteOldProfileImage($oldProfileImage);
-        $user->userInfo->update([
-          'profile_image' => $path,
-        ]);
-      } elseif ($oldProfileImage !== 'storage/user_profile_images/default-avatar.png') {
-        // No image is provided, and the old image is not the default image
-        $oldProfileImage = str_replace('storage/', '', $oldProfileImage);
-        Storage::disk('public')->delete($oldProfileImage);
-        $user->userInfo->update([
-          'profile_image' => 'storage/user_profile_images/default-avatar.png',
-        ]);
-      }
-      DB::commit();
-    } catch (\Exception $e) {
-      DB::rollBack();
-      if ($path) {
-        $path = str_replace('storage/', '', $path);
-        Storage::disk('public')->delete($path);
-      }
-      throw $e;
+    // Delete the existing image if it exists
+    $existingImage = $user->userInfo->getAttribute($imageType . '_image');
+    if ($existingImage && Storage::disk('public')->exists(str_replace('storage/', '', $existingImage))) {
+      Storage::disk('public')->delete(str_replace('storage/', '', $existingImage));
     }
+
+    // Convert backslashes to forward slashes in file path
+    $newImage = 'storage/' . str_replace('\\', '/', $newImage);
+
+    // Update the user info with the new image path
+    $userInfo = $user->userInfo;
+    $userInfo->{$imageType . '_image'} = $newImage;
+    $userInfo->save();
   }
 
-  private function deleteOldCoverImage($oldCoverImage) {
-    // Extract the path from the old image URL
-    $oldCoverImage = str_replace('storage/', '', $oldCoverImage);
+  // |--------------------------------------------------------------------------
+  /**
+   * Delete profile or cover image.
+   *
+   * @param \App\Models\UserInfo $userInfo
+   * @param string $imageType
+   * @return void
+   */
+  public function deleteImage(User $user, string $imageType): void {
+    $userInfo = $user->userInfo;
 
-    // Delete the old image if not default image
-    if ($oldCoverImage !== 'user_cover_images/default-cover.jpg') {
-      Storage::disk('public')->delete($oldCoverImage);
-    }
-  }
+    // Check if the user information exists and has the specified image
+    if ($userInfo && $userInfo->{$imageType . '_image'}) {
+      // Get the path of the existing image
+      $existingImage = $userInfo->{$imageType . '_image'};
 
-  public function updateCoverImage($user, $validatedRequest) {
-    try {
-      $path = null;
-      $oldCoverImage = $user->userInfo->cover_image;
-      DB::beginTransaction();
-
-      if (isset($validatedRequest['cover_image'])) {
-        // New image is provided
-        $path = Storage::disk('public')->put('user_cover_images', $validatedRequest['cover_image']);
-        $path = 'storage/' . str_replace('\\', '/', $path);
-        $this->deleteOldCoverImage($oldCoverImage);
-        $user->userInfo->update([
-          'cover_image' => $path,
-        ]);
-      } elseif ($oldCoverImage !== 'storage/user_cover_images/default-cover.jpg') {
-        // No image is provided, and the old image is not the default image
-        $oldCoverImage = str_replace('storage/', '', $oldCoverImage);
-        Storage::disk('public')->delete($oldCoverImage);
-        $user->userInfo->update([
-          'cover_image' => 'storage/user_cover_images/default-cover.jpg',
-        ]);
+      // Check if the image exists
+      if (Storage::disk('public')->exists(str_replace('storage/', '', $existingImage))) {
+        // Delete the image
+        Storage::disk('public')->delete(str_replace('storage/', '', $existingImage));
       }
-      DB::commit();
-    } catch (\Exception $e) {
-      DB::rollBack();
-      if ($path) {
-        $path = str_replace('storage/', '', $path);
-        Storage::disk('public')->delete($path);
-      }
-      throw $e;
+
+      // Set the image path to null
+      $userInfo->{$imageType . '_image'} = null;
+      $userInfo->save();
     }
   }
 }
